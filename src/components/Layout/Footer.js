4eea26cfbfa594ad907d87370d95eecf14ecdc4f
Kaustuv Mishra <kaustuvmishra1@gmail.com>
2024-08-13 23:04:00 +0530

    Initial commit

---
+++ src/components/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {footer} from './Layout.module.scss';
+
+const Footer = () => (
+    <footer className={footer}>
+        <p>Author: Kaustuv Mishra,
+            LinkedIn <a href='https://www.linkedin.com/in/kaustuv-mishra-787684214/' target='_blank' rel='noopener noreferrer'>click here</a>
+        </p>
+    </footer>
+);
+
+export default Footer;
